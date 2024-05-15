8dc119bdf80e03298d9255f4d87d82fbe018bec5
Stas <stas.shc@gmail.com>
2024-05-16 02:24:18 +0300

    fix: center

--- Calc.js
+++ Calc.js
@@ -1,16 +1,16 @@
-const e0 = 8.85e-12
+const e0 = 8.85 * 1e-12
 function changeE(x) {
     let sigma = parseInt(document.getElementById("Ro").value)
     let r1 = parseInt(document.getElementById("InnerRadius").value)
     let r = parseInt(document.getElementById("OuterRadius").value)
-    document.getElementById("e").innerHTML = sigma * x / (2 * e0) * ((1 / Math.sqrt(x*x + r1*r1) - 1 / Math.sqrt(x*x + r*r)))
+    document.getElementById("e").innerHTML = (sigma * x / (2 * e0) * ((1 / Math.sqrt(x*x + r1*r1) - 1 / Math.sqrt(x*x + r*r)))) / 1000
 }
 
 function changePhi(x) {
     let sigma = parseInt(document.getElementById("Ro").value)
     let r1 = parseInt(document.getElementById("InnerRadius").value)
     let r = parseInt(document.getElementById("OuterRadius").value)
-    document.getElementById("phi").innerHTML = sigma / (2 * e0) * (Math.sqrt(x*x + r*r) - Math.sqrt(x*x + r1*r1))
+    document.getElementById("phi").innerHTML = (sigma / (2 * e0) * (Math.sqrt(x*x + r*r) - Math.sqrt(x*x + r1*r1))) / 1000
 }
 
 function changeX(x) {
@@ -18,7 +18,7 @@ function changeX(x) {
 }
 
 function coordinate(event) {
-    let x = (event.clientX - 642);
+    let x = (event.clientX - window.innerWidth/2);
     changeX(x);
     changeE(x);
     changePhi(x);
